c97c33e2f63ae88b2a5fe04e2f83dc34fcf3dd5c
lumaldonado <lulimaldonado98@hotmail.com>
2023-03-07 17:52:19 -0300

    base clase inicial camel

---
+++ src/main/java/org/example/CamelRest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelRest extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+
+
+    }
+}
